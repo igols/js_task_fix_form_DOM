24816c5e0d62701d51c0f1a71ce993c868e4d2de
Igor <igor.olshanskyi@gmail.com>
2024-10-21 13:02:37 +0300

    make build

---
+++ index.fdbec3d8.js
@@ -0,0 +1,2 @@
+document.querySelectorAll("input").forEach(function(e){var t,l=document.createElement("label");l.classList.add("field-label"),l.setAttribute("for",e.id),l.textContent=e.getAttribute("name"),e.setAttribute("placeholder",(t=e.getAttribute("name")).at(0).toUpperCase()+t.slice(1)),e.before(l)});
+//# sourceMappingURL=index.fdbec3d8.js.map
